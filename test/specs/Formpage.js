c583860834804cdcdd6550da25c4e434c4d3283b
Pavan Kumar <pavankumartvs123@gmail.com>
2019-06-30 20:23:29 +0530

    This is first comit

---
+++ test/specs/Formpage.js
@@ -0,0 +1,5 @@
+import loginpage from basic;
+class Formpage extends Loginpage{
+
+     a = $('.gLFyf gsfi').addvalue('SAP Basics');
+}
---
+++ test/specs/basic.js
@@ -0,0 +1,7 @@
+export default class Loginpage {
+
+   open(path){
+       Browser.url('www.google.com');
+   };
+
+}
